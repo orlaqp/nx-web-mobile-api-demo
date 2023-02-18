01140902ae6d7a3b4bcb8dceb46ca8f068942fb6
Orlando Quero <orlando.quero@mobiquity.com>
2023-02-18 12:14:28 -0500

    added api client library

--- scripts/generate-api-client.js
+++ scripts/generate-api-client.js
@@ -1,14 +1,12 @@
 const { exec } = require("child_process");
-const { readFileSync } = require("fs");
 const { download } = require("./download-file");
 
 const url = 'http://localhost:3333/api-json';
 const output = 'api.json';
+const folderOutput = 'libs/shared/api-client/src/lib'
 
-
-async function createClient() {
-    await download(url, output);
-    exec('npx openapi-generator-cli generate -i api.json -g typescript-axios -o generated', (error, stdout, stderr) => {
+async function runProcess(input, message) {
+    exec(input, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -18,26 +16,22 @@ async function createClient() {
             return;
         }
         
-        console.log('API Client library completed.');
+        console.log(message);
     })
 }
 
-async function createDocumentation() {
+async function generateCode() {
     await download(url, output);
-    exec('npx openapi-generator-cli generate -i api.json -g html2 -o generated', (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        
-        console.log('API Client Documentation completed.');
-    })
+    runProcess(
+        `npx openapi-generator-cli generate -i api.json -g typescript-axios -o ${folderOutput}`,
+        'API Client library completed.'
+    );
+    runProcess(
+        `npx openapi-generator-cli generate -i api.json -g html2 -o ${folderOutput}`,
+        'API Documentation completed.'
+    );
 }
 
-createClient();
-createDocumentation();
+generateCode();
+
 
